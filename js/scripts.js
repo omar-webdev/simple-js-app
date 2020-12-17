8d7a00db65d2981b98658e094c33463ce1d258a3
omar-webdev <74170107+omar-webdev@users.noreply.github.com>
2020-12-17 07:16:42 +0100

    simple-js-app creation

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+alert('Hello world');
+
+let favoriteFood = 'couscous';
+document.write(favoriteFood);
+favoriteFood = 'pizza';
+document.write(favoriteFood);
+
+//some simple math
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
