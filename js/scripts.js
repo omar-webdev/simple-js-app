e35489ab9230504592f0f65967d2b9528b23e549
omar-webdev <74170107+omar-webdev@users.noreply.github.com>
2021-04-13 12:39:36 +0200

    for loop
    - a for loop that iterates over each item in pokemonList
    + description on size.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ let pokemonList = [
     {name:"Ivysaur", height:1, types:["grass","poison"]},
     {name:"Venusaur", height:2, types:["grass","poison"]}
 ];
+
+//loop
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height <1){
+        document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ") " + "that's a small pokemon." + "<br/>" + "<br/>")
+    }else if (pokemonList[i].height >=1 && pokemonList[i].height <2) {
+        document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ") " + "that's an average pokemon." + "<br/>" + "<br/>")
+    } else {
+        document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ") " + "that's a big pokemon." + "<br/>" + "<br/>")
+    }
+}
