5c53000939b6156fe3e08d624a98b6232e54704f
omar-webdev <74170107+omar-webdev@users.noreply.github.com>
2021-05-04 15:23:23 +0200

    forEach()
    forEach loop to iterate over the pokemons in the pokemonList in order to print their informations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,44 @@
-let pokemonList = [
-    {name:"Bulbasaur", height:0.7, types:["grass","poison"]},
-    {name:"Ivysaur", height:1, types:["grass","poison"]},
-    {name:"Venusaur", height:2, types:["grass","poison"]}
-];
+// let pokemonList = [
+//     {name:"Bulbasaur", height:0.7, types:["grass","poison"]},
+//     {name:"Ivysaur", height:1, types:["grass","poison"]},
+//     {name:"Venusaur", height:2, types:["grass","poison"]}
+// ];
 
-//loop
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height <1){
-        document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ") " + "that's a small pokemon." + "<br/>" + "<br/>")
-    }else if (pokemonList[i].height >=1 && pokemonList[i].height <2) {
-        document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ") " + "that's an average pokemon." + "<br/>" + "<br/>")
-    } else {
-        document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ") " + "that's a big pokemon." + "<br/>" + "<br/>")
-    }
-}
+// //loop
+// for (let i = 0; i < pokemonList.length; i++) {
+//     if (pokemonList[i].height <1){
+//         document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ") " + "that's a small pokemon." + "<br/>" + "<br/>")
+//     }else if (pokemonList[i].height >=1 && pokemonList[i].height <2) {
+//         document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ") " + "that's an average pokemon." + "<br/>" + "<br/>")
+//     } else {
+//         document.write(pokemonList[i].name + " (Height:" + pokemonList[i].height + ") " + "that's a big pokemon." + "<br/>" + "<br/>")
+//     }
+// }
 
+
+//forEach function
 pokemonList.forEach(function(pokemon){
     document.write(pokemon.name + ': ' + '</br>' + 'Height: '+ pokemon.height + '</br>' + 'Type: ' + pokemon.types + '<br>' + '<br>')
 });
-    
+  
+  
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name:"Bulbasaur", height:0.7, types:["grass","poison"]},
+        {name:"Ivysaur", height:1, types:["grass","poison"]},
+        {name:"Venusaur", height:2, types:["grass","poison"]}
+ ];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
